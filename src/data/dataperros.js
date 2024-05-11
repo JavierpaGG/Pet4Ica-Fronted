8df17b2375376597cb48f883a97e194646f92e5f
Alexis Diaz <alexisshande@gmail.com>
2024-05-11 18:52:44 -0500

    Pantalla de inicio

---
+++ src/data/dataperros.js
@@ -0,0 +1,18 @@
+export const pets= [
+    {
+        name: 'Juancho2 dog',
+        source: require('../../assets/pet1.jpg')
+    },
+    {
+        name: 'Pepe dog',
+        source: require('../../assets/pet2.jpg')
+    }, 
+    {
+        name: 'Tito dog',
+        source: require('../../assets/pet3.jpg')
+    }, 
+    {
+        name: 'Carlos dog',
+        source: require('../../assets/pet4.jpg')
+    },
+]
